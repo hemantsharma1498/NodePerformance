b336cf2d30b49e056211ff38d079aa1328e24185
hemantsharma1498 <hemantsharma1498@gmail.com>
2022-07-08 19:50:09 +0530

    benchmarking against pbkdf2 with 1 thread for workers

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //Dependencies
 const cluster=require('cluster');
 
+//Set single thread per threadpool 
+process.env.UV_THREADPOOL_SIZE=1;
 
 //Is the file being executed in master mode?
 if(cluster.isMaster){
 
-    //index.js will be executed again but in child mode (in in else clause)
+    //index.js will be executed again but in child mode (as in else clause)
     cluster.fork();
-
+    
 
 }else{
 
@@ -15,29 +17,29 @@ if(cluster.isMaster){
 
     const express=require('express');
     const app=express();
+    const crypto=require('crypto');
     
     
-    //Function to simulate a compute heavy task
-    let pseudoWork=function(duration){
-        const start=Date.now();
-    
-        while(Date.now()-start<duration){}
-    };
     
     
     
     
     //Basic routing
     app.get('/', function(req, res){
-    
-    
-        //Single instance of the event loop will take 5 seconds to resolve pseudoWork per call. 
-        //Multiple calls demonstrate how the event loop can get blocked up on compute heavy tasks    
-        pseudoWork(5000);
-        res.send('Hello world');
+        
+        //Using pbkdf2 of the crypto module to benchmark performance against blocking code
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', ()=>{
+            
+            res.send('Hello world');
+
+        });
+
     });
     
     
+    app.get('/fast', function(req, res){
+        res.send('Hello world, but fast');
+    })
     
     
     app.listen(3000);
